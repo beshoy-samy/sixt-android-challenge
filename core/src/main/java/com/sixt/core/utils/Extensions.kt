a794eede9f4032ae5bf751254d675acc3cf6aac3
Beshoy Samy <beshoy.samy411@gmail.com>
2021-09-30 21:34:02 +0200

    added some extension functions to Extensions.kt

--- core/src/main/java/com/sixt/core/utils/Extensions.kt
+++ core/src/main/java/com/sixt/core/utils/Extensions.kt
@@ -1,2 +1,22 @@
 package com.sixt.core.utils
 
+import android.content.Context
+import android.widget.ImageView
+import android.widget.Toast
+import androidx.annotation.DrawableRes
+import coil.load
+import com.sixt.core.R
+
+fun Context.shortToast(message: String) = Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
+
+fun Context.longToast(message: String) = Toast.makeText(this, message, Toast.LENGTH_LONG).show()
+
+fun ImageView.loadImage(
+    url: String?,
+    crossfade: Boolean = true,
+    @DrawableRes placeholder: Int = R.drawable.ic_image_placeholder
+) = load(url) {
+    crossfade(crossfade)
+    placeholder(placeholder)
+    error(placeholder)
+}
